97bb35918ea1b43f294742d70fa76ce4f1f344bd
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:44:58 +0300

    Initial commit

---
+++ Task1/Task1.cs
@@ -0,0 +1,32 @@
+﻿namespace Task1
+{
+    internal class Task1
+    {
+        public delegate void MathDelegate(int x);
+
+        public static int FactorialCounter(int y)
+        {
+            if (y < 0)
+                return -1;
+
+            if (y == 0)
+                return 1;
+
+            int result = 1;
+            for (int i = 1; i < y + 1; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            MathDelegate actions = (x) => Console.WriteLine($"Квадрат числа {x} = {x * x}");
+            actions += (x) => Console.WriteLine($"Факториал числа {x} = {FactorialCounter(x)}");
+            actions += (x) => Console.WriteLine($"Модуль числа {x} = {Math.Abs(x)}");
+
+            actions(8);
+        }
+    }
+}
---
+++ Task2/Task2.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        delegate void MathFunctions(int x, int y);
+
+        static void Main(string[] args)
+        {
+            MathFunctions function = (x, y) => Console.WriteLine($"Сумма {x} и {y} = {x + y}");
+            function += (x, y) => Console.WriteLine($"Разность {x} и {y} = {x - y}");
+            function += (x, y) => Console.WriteLine($"Произведение {x} и {y} = {x * y}");
+            function += (x, y) => Console.WriteLine($"Частное {x} и {y} = {x / y}");
+
+            function(13, 11);
+        }
+    }
+}
